247b909ba06b897630792af2fb0e1bd1a5e32459
Kacper Walter <kacper.walter@netguru.com>
2021-11-06 15:45:08 +0100

    start workin

--- 01/index.js
+++ 01/index.js
@@ -1,2 +1,11 @@
-// https://api.punkapi.com/v2/beers
+const API_URL = 'https://api.punkapi.com/v2/beers';
 
+const success = (data) => {
+  console.log(data);
+}
+
+const error = (err) => {
+  console.log(err);
+}
+
+const req = new XMLHttpRequest();
