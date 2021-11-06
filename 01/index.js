6e002e2cde7b6cdc8c47d4bc64f19383d6e90d9c
Kacper Walter <kacper.walter@netguru.com>
2021-11-06 16:37:49 +0100

    MVP works

--- 01/index.js
+++ 01/index.js
@@ -1,7 +1,28 @@
 const API_URL = 'https://api.punkapi.com/v2/beers';
+const container = document.querySelector('.container');
+
+const render = (data) => {
+  if (!data.length) return; // prevents running function if data is empty
+  const fragment = document.createDocumentFragment();
+  data.forEach(({ name, tagline, description, image_url: imageURL }) => {
+    const div = document.createElement('div');
+    div.classList.add('beer');
+    div.innerHTML = `
+    <div class="beer--content">
+      <h1 class="beer--title">${name}</h1>
+      <p class="beer--tagline">${tagline}</p>
+      <p class="beer--description">${description}</p>
+    </div>
+    <img class="beer--image" src="${imageURL}">
+    `;
+    fragment.appendChild(div);
+  });
+  container.appendChild(fragment);
+}
 
 const success = (data) => {
-  console.log(data);
+  const beers = JSON.parse(data.target.responseText);
+  render(beers);
 }
 
 const error = (err) => {
@@ -9,3 +30,8 @@ const error = (err) => {
 }
 
 const req = new XMLHttpRequest();
+req.onload = success; // req.addEventListener('load', success);
+req.onerror = error; // req.addEventListener('error', error);
+
+req.open('GET', API_URL);
+req.send();
